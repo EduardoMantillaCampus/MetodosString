bf61f89da832a3114b7a64d0f8b94c13349b9c27
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 09:33:38 -0500

    feat(new operator): :sparkles: Operadores de comparacion
    Documentacion acerca de los operadores de comparacion

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 //OPERADORES ARITMETICOS
 
 console.log(`
-Operadores Aritmeticos
+Operadores de Comparacion
 
-Un operador aritmético toma valores numéricos (ya sean literales o variables) como sus operandos y devuelve un solo valor numérico. Los operadores aritméticos estándar son suma (+), resta (-), multiplicación (*) y división (/). 
+1. Igualdad == 
+Devuelve true si los operandos son iguales.
 
-
-1. Suma
-Si en lugar de números se suman cadenas, como por ejemplo "A" + "B" = "AB" se produce una concatenación. Si alguno de los elementos a concatenar no es una cadena de texto, queda convertido a cadena: "AB" + 123 = "AB123" . 
-Ejemplo
-Suma dos números: 3 + 2 = 5
-
-2. Resta
-Halla la diferencia entre dos números. 
-Ejemplo 
-A: 3-2 = 1
-B: (-1)-(-2) = 1 
-
-3. Multiplicación
-Multiplica dos números
-EJemplo: 
-3 * 2 = 6
-
-4. División
-Divide dos números, obteniendo un cociente de coma flotante: 5 / 2 = 2.5 . Fíjate en que el separador decimal es el punto. 
-
-5. Residuo 
-Devuelve el resto entero de dividir los dos operandos.
 Ejemplo:
-let dato= 12%5
-12 % 5 = 2
+3 == 3 -> True
+"3"== 3 -> False
+
+2. Desigualdad != 
+Devuelve true si los operandos no son iguales.
+var1 != 4
+var2 != "3"
+
+3. Desigualdad estricta 
+Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.
+
+var1 !== "3"
+3 !== '3'
+
+4. Mayor que >
+Devuelve true si el operando izquierdo es mayor que el operando derecho.
+
+var2 > var1 "12" > 2
+
+5. Mayor igual que >=
+Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.
+
+var2 >= var1 var1 >= 3
+
+6. Menor que <
+Devuelve true si el operando izquierdo es menor que el operando derecho
+
+var2 < var1 "12" < 2
+
+7. Menor igual que <=
+Devuelve true si el operando izquierdo es menor o igual que el operando derecho
+
+var2 <= var1 var1 <= 3
 
 `)
