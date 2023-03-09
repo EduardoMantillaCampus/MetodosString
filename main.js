4b948a76e57afdb984f82dc1bfd6c63819e6922b
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 06:54:53 -0500

    fromCharCode()

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-//3. codePointAt
+//4. fromCharCode()
 console.log(`
-Metodo --> codePointAt()
+Metodo --> fromCharCode()
 
-Definicion -->  El codePointAt() método devuelve un entero no negativo que es el valor del punto de código Unicode en la posición dada. Tenga en cuenta que esta función no proporciona el punto de código n en una cadena, sino el punto de código que comienza en el índice de cadena especificado.
+Definicion --> El String.fromCharCode() método estático devuelve una cadena creada a partir de la secuencia especificada de unidades de código UTF-16.
 
 Ejemplo`)
-const icons = '☃★♲'
-console.log(icons.codePointAt(1))
+console.log(String.fromCharCode(189, 43, 190, 61))
