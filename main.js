80c5400516ff252dd5a619c9a2fe07a4c21c9e2e
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 09:30:24 -0500

    feat(new operator): :sparkles: Operadores Aritmeticos
    Documentacion acerca de los operadores aritmeticos

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+//OPERADORES ARITMETICOS
+
 console.log(`
-Metodo --> toUpperCase()
+Operadores Aritmeticos
 
-Definicion -->  El método toUpperCase() devuelve el valor de la cadena de llamada convertido a mayúsculas (el valor se convertirá en una cadena si no lo es).
+Un operador aritmético toma valores numéricos (ya sean literales o variables) como sus operandos y devuelve un solo valor numérico. Los operadores aritméticos estándar son suma (+), resta (-), multiplicación (*) y división (/). 
 
-Ejemplo`)
 
-let cadena =
- 'Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal'
+1. Suma
+Si en lugar de números se suman cadenas, como por ejemplo "A" + "B" = "AB" se produce una concatenación. Si alguno de los elementos a concatenar no es una cadena de texto, queda convertido a cadena: "AB" + 123 = "AB123" . 
+Ejemplo
+Suma dos números: 3 + 2 = 5
 
-console.log(cadena.toUpperCase())
+2. Resta
+Halla la diferencia entre dos números. 
+Ejemplo 
+A: 3-2 = 1
+B: (-1)-(-2) = 1 
+
+3. Multiplicación
+Multiplica dos números
+EJemplo: 
+3 * 2 = 6
+
+4. División
+Divide dos números, obteniendo un cociente de coma flotante: 5 / 2 = 2.5 . Fíjate en que el separador decimal es el punto. 
+
+5. Residuo 
+Devuelve el resto entero de dividir los dos operandos.
+Ejemplo:
+let dato= 12%5
+12 % 5 = 2
+
+`)
