b94d8f7528d8f5ca1de05eb7a4af64b8b45b6378
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 07:45:23 -0500

    normalize()

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 console.log(`
-Metodo --> matchAll()
+Metodo --> normalize()
 
-Definicion -->  El método matchAll() retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, incluyendo grupos de captura.
+Definicion -->  El método normalize() devuelve la forma de normalización Unicode de la cadena.
 
 Ejemplo`)
 
-const regexp = /t(e)(st(\d?))/g
-const str = 'test1test2'
-
-const array = [...str.matchAll(regexp)]
-console.log(array[0])
+let cadena =
+ 'Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal'
+console.log(cadena.normalize('NFKD'))
