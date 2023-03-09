b87fbe3159c9119bbef5ddb3f46b59e167d5ac0f
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 07:54:40 -0500

    toLowerCase()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 console.log(`
-Metodo --> padEnd()
+Metodo --> toLowerCase()
 
-Definicion -->  El método padEnd() devuelve la forma de normalización Unicode de la cadena.
+Definicion -->  El método toLowerCase() devuelve el valor de la cadena de llamada convertido a minúsculas.
 
 Ejemplo`)
 
-const str1 = 'Breaded Mushrooms'
-console.log(str1.padEnd(25, '.'))
-const str2 = '200'
-console.log(str2.padEnd(5))
+let cadena =
+ 'Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal'
+
+console.log(cadena.toLowerCase())
