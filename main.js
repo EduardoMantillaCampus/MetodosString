991fcbc6ea75e46945cef1cc1f527f73b609141f
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 06:50:21 -0500

    charAt()

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ console.log(`
 Metodo --> charAt()
 
 Definicion --> El charAt()método de una Stringinstancia devuelve una nueva cadena que consta de la única unidad de código UTF-16 ubicada en el desplazamiento especificado en la cadena.
+
+Texto --> Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal
+
+Ejemplo`)
+const charAt =
+ 'Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal'
+const index1 = 4
+console.log(
+ `El caracter encontrado en la posicion ${index1} es ${charAt.charAt(index1)}`
+)
