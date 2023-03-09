78e6715b150d2905e75ecd5b054d4da461019b9e
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 06:45:28 -0500

    Inicio actividad

---
+++ main.js
@@ -0,0 +1,5 @@
+//1. charAt
+console.log(`
+Metodo --> charAt()
+
+Definicion --> El charAt()método de una Stringinstancia devuelve una nueva cadena que consta de la única unidad de código UTF-16 ubicada en el desplazamiento especificado en la cadena.
