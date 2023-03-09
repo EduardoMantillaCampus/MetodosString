3b33abf1c586c9f74c274a59be78c9be88344197
Nicolas caicedo <caicedonicolas150@gmail.com>
2023-03-09 07:03:26 -0500

    Includes()

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-//8. endsWith()
+//9. Includes()
 console.log(`
-Metodo --> endsWith()
+Metodo --> Includes()
 
-Definicion --> El endsWith() método determina si una cadena termina con los caracteres de una cadena especificada, regresando true o false según corresponda.
+Definicion --> El includes() método realiza una búsqueda que distingue entre mayúsculas y minúsculas para determinar si una cadena se puede encontrar dentro de otra cadena, regresando trueo falsesegún corresponda.
 
 Ejemplo`)
-const str1e =
+const sentence =
  'Los productos mayor comercializados en colombia son los cosmeticos y productos de cuidado personal'
-console.log(str1e.endsWith('personal'))
-console.log(str1e.endsWith('cosmeticos', 17))
+const word = 'productos'
+console.log(
+ `La palabra "${word}" ${
+  sentence.includes(word) ? 'si se encuentra' : 'no '
+ } en la sentencia`
+)
